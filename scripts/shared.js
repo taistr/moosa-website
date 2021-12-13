ae9a7585dd6ef6379ceaebbcc2b11f717aef4c51
Tyson Truong <tystruong@gmail.com>
2021-12-13 14:20:20 +1100

    Added transitions for the menu

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,9 +1,13 @@
 "use strict"
 
 function toggleMenu() {
+    let body = document.querySelector("body");
     let menu = document.querySelector(".menu");
     let main = document.querySelector("main");
+    let menu_button = document.querySelector(".menu-button");
 
+    body.classList.toggle("active");
     menu.classList.toggle("active");
     main.classList.toggle("active");
+    menu_button.classList.toggle("active");
 }
